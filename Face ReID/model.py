f2b6ceda0bac9c267d2238ee22691269d9f2fa10
Andrewzh112 <andrewzhao112@gmail.com>
2020-08-17 00:25:46 -0700

    fixed init

--- Face ReID/model.py
+++ Face ReID/model.py
@@ -3,20 +3,21 @@ from torch import nn
 
 
 class CoupleFaceNet(nn.Module):
-    def __init__(self, hidden_size=256, latent_size=64):
+    def __init__(self, hidden_size=256, latent_size=64, pretrained=True):
         """
         Batch Mining Inspiration:
         https://arxiv.org/pdf/1703.07737.pdf
         """
         super(CoupleFaceNet, self).__init__()
-        self.feature2latent = models.resnet50(pretrained=True)
+        self.feature2latent = models.resnet50(pretrained=pretrained)
         projector = nn.Sequential(
             nn.Linear(self.feature2latent.fc.in_features, hidden_size),
             nn.ReLU(),
             nn.Linear(hidden_size, latent_size)
         )
         self.feature2latent.fc = projector
-        self.reset_params()
+        if not pretrained:
+            self.reset_params()
 
     def forward(self, images):
         latents = self.feature2latent(images)
@@ -25,13 +26,13 @@ class CoupleFaceNet(nn.Module):
     def reset_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                init.kaiming_normal(m.weight, mode='fan_out')
+                nn.init.kaiming_normal(m.weight, mode='fan_out')
                 if m.bias is not None:
-                    init.constant(m.bias, 0)
+                    nn.init.constant(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
-                init.constant(m.weight, 1)
-                init.constant(m.bias, 0)
+                nn.init.constant(m.weight, 1)
+                nn.init.constant(m.bias, 0)
             elif isinstance(m, nn.Linear):
-                init.normal(m.weight, std=0.001)
+                nn.init.normal(m.weight, std=0.001)
                 if m.bias is not None:
-                    init.constant(m.bias, 0)
+                    nn.init.constant(m.bias, 0)
